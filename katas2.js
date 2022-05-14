e679a877a5222c4e859515a46ed0463e105abc53
Luis Gabriel Modena <luisgabriel.modena326@gmail.com>
2022-05-14 20:29:03 -0300

    katas2

--- katas2.js
+++ katas2.js
@@ -1,37 +1,52 @@
-// comece a criar a sua função add na linha abaixo
+// Exercício 1
+function add(valor1, valor2) {
+    return valor1 + valor2
+}
+console.log(add(2, 4))
 
 
-// descomente a linha seguinte para testar sua função
-// console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
+// Exercício 2
+function multiply(valor1, valor2) {
+    let resultado = 0
+    for (let i = 0; i < valor2; i++) {
+        resultado += add(0, valor1)
+    }
+    return resultado
+}
+console.log(multiply(6, 8))
 
 
-// comece a criar a sua função multiply na linha abaixo
+// Exercício 3
+function power(valor1, valor2) {
+    let resultado = [valor1]
+    for (let i = 0; i < valor2 - 1; i++) {
+        resultado.push(multiply(valor1, resultado[i]))
+    }
+    return resultado[resultado.length - 1]
+}
+
+console.log(power(2, 8))
 
 
-// descomente a linha seguinte para testar sua função
-// console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+// Exercício 4
+function factorial(valor1) {
+    let resultado = 1
+    for (let i = valor1; i >= 1; i--) {
+        resultado = multiply(resultado, i)
+    }
+    return resultado
+}
+console.log(factorial(4))
 
 
-// comece a criar a sua função power na linha abaixo
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
-
-
-// comece a criar a sua função factorial na linha abaixo
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
-
-
-/**
- * BONUS (aviso: o grau de dificuldade é bem maior !!!)
- */
-
-// crie a função fibonacci
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(fibonacci(7) === 13, 'A função fibonacci não está funcionando como esperado');
+// Exercício 5 (Bonus)
+function fibonacci(valor1) {
+    var fibonacci = [];
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
+    for (var i = 2; i < valor1; i++) {
+        fibonacci[i] = add([i] - 2, [i] - 1);
+    }
+    return fibonacci;
+}
+console.log(fibonacci(7))
